9a9c98c225d8cc04749303af65a99ca40aaf28a4
RomainMAURICE <78416232+RomainMAURICE@users.noreply.github.com>
2023-04-20 11:34:41 +0200

    Add files via upload

---
+++ TP6Algo/Base/concordance.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+#define MAX_WORD_LENGTH 80
+
+link *read_text(FILE *infile) {
+    link *lst = NULL;
+    char *word = (char *)malloc(MAX_WORD_LENGTH*sizeof(char));
+    while (fscanf(infile, "%s ", word) != -1) {
+
+        if(find_list(lst, word)!=NULL){
+            find_list(lst, word)->occurrences++;
+        }else{
+            lst = insert_first_list(lst, word);
+        }
+
+    }
+    free(word);
+    return lst;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: concordance <in_file>\n");
+        return 1;
+    }
+
+    FILE *fin = fopen(argv[1], "r");
+    if (fin == NULL) {
+        fprintf(stderr, "Error opening file for reading: %s\n", argv[1]);
+        return 1;
+    }
+
+    link *lst = read_text(fin);
+    fclose(fin);
+
+    display_list(lst);
+    
+    int words = 0;
+    int occurence = nb_occurence(lst);
+    link *ptr;
+    for (ptr = lst; ptr != NULL; ptr = ptr->next) {
+        words++;
+    }
+
+    printf("total number of words = %d\n", words);
+
+    free_list(lst);
+
+    return 0;
+}
---
+++ TP6Algo/Base/list.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+ * Create a link representing 1 occurence of the string 'word'.
+ * The string is copied and must be freed when the link is freed.
+ */
+link *create_link(char word[], link *next) {
+    link *lnk = malloc(sizeof(link));
+    lnk->word = malloc(strlen(word)+1);
+    strcpy(lnk->word, word);
+    lnk->occurrences = 1;
+    lnk->next = next;
+    return lnk;
+}
+
+void free_link(link *lnk) {
+    free(lnk->word);
+    free(lnk);
+}
+
+void display_list(link *lst) {
+    while (lst) {
+        printf("%s : %d\n", lst->word, lst->occurrences);
+        lst = lst->next;
+    }
+}
+
+void free_list(link *lst) {
+    while (lst) {
+        link *tmp = lst;
+        lst = lst->next;
+        free_link(tmp);
+    }
+}
+
+int equal(char* w1, char* w2) {
+    return strcmp(w1, w2) == 0;
+}
+
+link *find_list(link* lst, char word[]) {
+    link *ptr = lst;
+    while (ptr != NULL && !equal(ptr->word, word))
+        ptr = ptr->next;
+    return ptr;
+}
+
+link *insert_first_list(link *lst, char word[]) {
+    return create_link(word, lst);
+}
